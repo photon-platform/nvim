bee3cc3998434b409ead0923ba421a5d287cb106
phiarchitect <phi@phiarchitect.com>
2025-04-30 08:05:12 -0700

    configured

---
+++ install_neovim.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo install npm and nodejs
+echo
+sudo apt install -y npm nodejs
+
+echo
+echo npm
+npm -v
+echo node
+node -v
+echo
+
+cd ~/Downloads
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
